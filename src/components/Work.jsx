9c9cd3944336e6b8e8de1c63649fb4abdcbbc52d
Bontha Hariswar Reddy <hariswarreddy@Hariswar-Reddy-Bs-MacBook-Pro.local>
2025-04-11 12:08:30 +0530

    Made small changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -9,28 +9,28 @@ const Work = () => {
       <h2>Work</h2>
       <section>
         <article>
-                  <Carousel
-                  showArrows={false}
-                  // showIndicators={false}
-                      showThumbs={false}
-                      showStatus={false}
-                      interval={1000}
-                      infiniteLoop
-                      autoPlay
-                  >
-            
-              {data.projects.map((i) => (
-                <div className="workItem">
-                  {/* <img src={i.imgSrc} alt={i.title} /> */}
-                  <aside>
-                    <h3>{i.title}</h3>
-                    <p>{i.description}</p>
-                    <a target="blank" href={i.url}>
-                      View Demo
-                    </a>
-                  </aside>
-                </div>
-              ))}
+          <Carousel
+            showArrows={false}
+            // showIndicators={false}
+            showThumbs={false}
+            showStatus={false}
+            interval={2000}
+            stopOnHover
+            infiniteLoop
+            autoPlay
+          >
+            {data.projects.map((i) => (
+              <div className="workItem">
+                {/* <img src={i.imgSrc} alt={i.title} /> */}
+                <aside>
+                  <h3>{i.title}</h3>
+                  <p>{i.description}</p>
+                  <a target="blank" href={i.url}>
+                    View Demo
+                  </a>
+                </aside>
+              </div>
+            ))}
           </Carousel>
         </article>
       </section>
